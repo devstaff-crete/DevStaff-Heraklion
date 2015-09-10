46c4d7a6a8aa3a02991d52edd51e1b8b6a7052ca
Nikos Kampitakis <nikosk@imgzine.com>
2015-09-10 19:14:13 +0300

    added get /tasks route

--- meetups/meetup02-NodeJS/devstaff-api-node/index.js
+++ meetups/meetup02-NodeJS/devstaff-api-node/index.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var app = express();
 
+var dbCollection = [
+  {id: '0', title: 'my first task', cdate: '2015-09-08T18:39:37+03:00', desc: 'I have to finish this first task'},
+  {id: '1', title: 'another task', cdate: '2015-09-09T20:41:37+03:00', desc: 'I can skip this one'},
+  {id: '2', title: 'get beers', cdate: '2015-09-10T08:39:37+03:00', desc: 'Do not forget the beers'}
+];
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.get('/tasks', function (req, res, next) {
+  res.setHeader('Content-Type', 'text/json');
+  res.end(JSON.stringify(dbCollection));
 });
 
 var server = app.listen(3000, function () {
