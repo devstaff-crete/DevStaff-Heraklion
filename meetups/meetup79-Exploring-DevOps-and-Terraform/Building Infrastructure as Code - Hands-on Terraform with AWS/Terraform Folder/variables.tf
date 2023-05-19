18451f33e0e54dbb9cfe62fe375509f85312accd
ZERVAKISG <g.zervakis@elsevier.com>
2023-05-19 10:49:41 +0100

    Building Infrastructure as Code - Hands-on Terraform with AWS - Slides & Code

---
+++ meetups/meetup79-Exploring-DevOps-and-Terraform/Building Infrastructure as Code - Hands-on Terraform with AWS/Terraform Folder/variables.tf
@@ -0,0 +1,25 @@
+variable "cidr_block" {
+  description = "CIDR of VPC"
+  default     = "10.0.0.0/24"
+  type        = string
+}
+
+variable "vpc_name" {
+
+}
+
+variable "tag_product" {
+
+}
+
+variable "tag_description" {
+
+}
+
+variable "tag_orchestration" {
+
+}
+
+variable "tag_environment" {
+
+}
